b992d50dd2fe0305cf85ece5f0d3f1f14b55691b
DavidPavach <DavidPavach@proton.me>
2026-05-30 21:09:15 +0100

    fix: contact page

--- src/routes/_home/contact.tsx
+++ src/routes/_home/contact.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Contact from "@/pages/Home/Contact";
+import { APP_NAME } from "../__root";
 
 export const Route = createFileRoute("/_home/contact")({
+	head: () => ({
+		meta: [
+			{
+				title: `Contact | ${APP_NAME}`,
+			},
+		],
+	}),
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/_home/contact"!</div>;
+	return <Contact />;
 }
